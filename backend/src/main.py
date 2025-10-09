ed730470a36752171239e0a31a1b89a8295ef173
Jirayu Kaewsing <jirayukaewsing48@gmail.com>
2025-10-10 03:20:03 +0700

    feat(backend): Add log hook on request

--- backend/src/main.py
+++ backend/src/main.py
@@ -1,5 +1,12 @@
-from fastapi import FastAPI
+import time
+
+import logging
+from logging.handlers import RotatingFileHandler
+
+from fastapi import FastAPI, Request
 from fastapi.middleware.cors import CORSMiddleware
+
+from starlette.middleware.base import BaseHTTPMiddleware
 
 from routes.healthcheck import healthcheck_route
 from routes.auth import auth_route
@@ -10,7 +17,32 @@ from routes.dashboard import dashboard_router
 
 from db import connect_db, disconnect_db
 
+LOG_FILE = "/var/backend/logs/api.log"
+
+file_handler = RotatingFileHandler(LOG_FILE, maxBytes=10*1024*1024, backupCount=5)
+file_handler.setFormatter(logging.Formatter(
+    '%(asctime)s %(levelname)s: %(message)s',
+    '[in %Y-%m-%d %H:%M:%S]' # Timestamp format for easy parsing
+))
+
+logger = logging.getLogger("api_logger")
+logger.setLevel(logging.INFO)
+logger.addHandler(file_handler)
+
+class LogRequestsMiddleware(BaseHTTPMiddleware):
+    
+    async def dispatch(self, request: Request, call_next):
+        
+        logger.info(f"request path='{request.url.path}' method='{request.method}'")
+        response = await call_next(request)
+        
+        return response
+
+
 app = FastAPI()
+
+app.add_middleware(LogRequestsMiddleware)
+
 app.include_router(healthcheck_route, prefix="/api/v1")
 app.include_router(auth_route, prefix="/api/v1")
 app.include_router(oauth_route, prefix="/api/v1")
